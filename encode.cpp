d4cfc97f2feb9f3e038ccc97c2ae0f140b56b4c2
Masen Lunt <masen.lunt@gmail.com>
2021-09-06 18:37:00 -0700

    added insertion sort

--- encode.cpp
+++ encode.cpp
@@ -13,7 +13,16 @@ string shift(string s) {
 }
 
 void insertionSort(string s[]) {
-
+    int arr_len = s[0].size(); // assumes that the array was constructed by shifting string of len n, n times
+    for(int ii=1; ii<arr_len; ii++) {
+        string key = s[ii];
+        int jj = ii - 1;
+        while(ii>=0 and s[ii][arr_len-1] > key[arr_len-1]) {
+            s[jj+1] = s[jj];
+            jj -= 1;
+        }
+        s[jj+1] = key;
+    }
 }
 
 int main() {
@@ -26,6 +35,7 @@ int main() {
     for(int ii=1; ii<input.size(); ii++){
         input_arr[ii] = shift(input_arr[ii - 1]);
     }
+    insertionSort(input_arr);
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,3 @@
-firstExecute: main.cpp
-	g++ -std=c++11 -o executable main.cpp
-
 firstExecute: encode.cpp
 	g++ -std=c++11 -o encode encode.cpp
 
