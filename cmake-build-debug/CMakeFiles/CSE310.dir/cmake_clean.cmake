b9d5ca83438cd2c503628b25f504e2737309b45c
Masen Lunt <masen.lunt@gmail.com>
2021-09-09 10:39:07 -0700

    make insertionSort using pointers

---
+++ cmake-build-debug/CMakeFiles/CSE310.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CSE310.dir/main.cpp.o"
+  "CSE310.pdb"
+  "CSE310"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CSE310.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ cmake-build-debug/CMakeFiles/CSE310.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CSE310.
+# This may be replaced when dependencies are built.
