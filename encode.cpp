0db7ebf7fb3d9103efafad1b93e58f75f27b0b20
Masen Lunt <masen.lunt@gmail.com>
2021-09-10 19:17:52 -0700

    WORKING!!!!

--- encode.cpp
+++ encode.cpp
@@ -12,48 +12,44 @@ string shift(string s) {
     return s;
 }
 
-void insertionSort(string s[]) {
-    int arr_len = s[0].size(); // assumes that the array was constructed by shifting string of len n, n times
-    for(int ii=1; ii<arr_len; ii++) {
-        string key = s[ii];
+void insertionSort(char** s, int n) {
+    for(int ii=1; ii<n; ii++) {
+        char* temp = s[ii];
         int jj = ii - 1;
-        while(ii>=0 and s[jj][arr_len-1] > key[arr_len-1]) {
+        while(ii>=0 and s[jj][n-1] > temp[n-1]) {
             s[jj+1] = s[jj];
             jj -= 1;
         }
-        s[jj+1] = key;
+        s[jj+1] = temp;
     }
 }
-/*
-void insertionSort(string s[]) {
-    int arr_len = s[0].size(); // assumes that the array was constructed by shifting string of len n, n times
-    for(string* outer_str_ptr = &s[0]; outer_str_ptr <= &s[arr_len-1]; outer_str_ptr++) {
-        string key = *outer_str_ptr;
-        string* inner_str_ptr = outer_str_ptr - arr_len;
-        // build case if char is == key, sort based on next letter
-        while(outer_str_ptr>=&s[0] and (*outer_str_ptr)[arr_len-1] > key[arr_len-1]) {
-            *(inner_str_ptr + arr_len) = *inner_str_ptr;
-            inner_str_ptr--;
-        }
-        *(inner_str_ptr + arr_len) = key;
-    }
-}
-*/
+
 int main() {
     string input;
     getline(cin, input);
 
-    string input_arr[input.size()];
-    input_arr[0] = input;
+    int size = input.size();
 
+    char **ptr_lst = new char*[size];
+    for(int ii=0; ii<size; ii++) {
+        ptr_lst[ii] = new char[size];
+        for(int jj=0; jj<size; jj++) {
+            ptr_lst[ii][jj] = input[jj];
+        }
+        input = shift(input);
+    }
 
-    for(int ii=1; ii<input.size(); ii++){
-        input_arr[ii] = shift(input_arr[ii - 1]);
+    insertionSort(ptr_lst, size);
+
+    for(int ii=0; ii<size; ii++) {
+        for(int jj=0; jj<size; jj++) {
+            cout << ptr_lst[ii][jj];
+        }
+        cout << endl;
     }
-    insertionSort(input_arr);
-    for(string* arr_start = &input_arr[0]; arr_start <= &input_arr[input.size() - 1]; arr_start++){
-        cout << *arr_start << endl;
-    }
+
     return 0;
 }
+
+
 
