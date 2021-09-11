39d884301d4dafac54579c36d1028f17658ff9a4
Masen Lunt <masen.lunt@gmail.com>
2021-09-11 15:17:33 -0700

    just need to cross compare if strings are == at searching index

--- encode.cpp
+++ encode.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 string shift(string s) {
-    char ref = s[s.size() - 1];
-    for(int ii=s.size()-1; 0<ii; ii--){
-        s[ii] = s[ii-1];
+    char ref = s[int(s.size()) - 1];
+    for(int ii=int(s.size()-1); 0<ii; ii--){
+        s.at(ii) = s.at(ii-1);
     }
-    s[0] = ref;
+    s.at(0) = ref;
     return s;
 }
 
@@ -16,7 +16,7 @@ void insertionSort(char** s, int n) {
     for(int ii=1; ii<n; ii++) {
         char* temp = s[ii];
         int jj = ii - 1;
-        while(ii>=0 and s[jj][n-1] > temp[n-1]) {
+        while(jj>=0 and s[jj][n-1] > temp[n-1]) {
             s[jj+1] = s[jj];
             jj -= 1;
         }
@@ -24,28 +24,40 @@ void insertionSort(char** s, int n) {
     }
 }
 
-int main() {
-    string input;
-    getline(cin, input);
+int main(int argc, char * argv[]) {
+    if (argc > 1) {
+        if (string(argv[1]) == "insertion") {
+            string input;
+            while (getline(cin, input)) {
+                int size = int(input.size());
 
-    int size = input.size();
+                char **ptr_lst = new char*[size];
+                for(int ii=0; ii<size; ii++) {
+                    ptr_lst[ii] = new char[size];
+                    for(int jj=0; jj<size; jj++) {
+                        ptr_lst[ii][jj] = input.at(jj);
+                    }
+                    input = shift(input);
+                }
 
-    char **ptr_lst = new char*[size];
-    for(int ii=0; ii<size; ii++) {
-        ptr_lst[ii] = new char[size];
-        for(int jj=0; jj<size; jj++) {
-            ptr_lst[ii][jj] = input[jj];
+                insertionSort(ptr_lst, size);
+
+                for(int ii=0; ii<size; ii++) {
+                    for (int jj = 0; jj < size; jj++) {
+                        cout << ptr_lst[ii][jj];
+                    }
+                    cout << endl;
+                }
+
+                for(int ii=0; ii<size; ii++) {
+                    delete[] ptr_lst[ii];
+                }
+                delete[] ptr_lst;
+            }
         }
-        input = shift(input);
-    }
-
-    insertionSort(ptr_lst, size);
-
-    for(int ii=0; ii<size; ii++) {
-        for(int jj=0; jj<size; jj++) {
-            cout << ptr_lst[ii][jj];
+        else if (string(argv[1]) == "mergesort") {
+            // come back after milestone
         }
-        cout << endl;
     }
 
     return 0;
